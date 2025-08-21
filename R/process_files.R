b12ffc1eb1d971ae6c92563a545c4f924d6a47c9
Trizzole <thomasereinke@gmail.com>
2025-08-21 10:13:17 -0500

    move report params across multiple lines

--- R/process_files.R
+++ R/process_files.R
@@ -25,7 +25,12 @@
 #' temp_dir <- tempfile("example-")
 #' dir.create(temp_dir)
 #'
-#' report_params <- tidyr::expand_grid(chapter = 1, problem_numbers = 1:2, author = "Firstname Lastname")
+#' report_params <- tidyr::expand_grid(
+#'   chapter = 1,
+#'   problem_numbers = 1:2,
+#'   author = "Firstname Lastname"
+#' )
+#'
 #' rmd_files <- generate_reports(
 #'   params_df = report_params,
 #'   template_name = "simple_report",
